ddaf9d1577235de58812def8a44db12b319ef6ac
Tchasinga jacques <tchasingajacques@gmail.com>
2023-12-20 13:26:30 +0200

    Commiting a new changes

--- frontend/src/Pages/Sharingdeteals.jsx
+++ frontend/src/Pages/Sharingdeteals.jsx
@@ -6,6 +6,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 import "swiper/css/bundle";
 import SwiperCore from "swiper";
+import {FaShare,} from 'react-icons/fa';
 
 export default function Sharingdeteals() {
   SwiperCore.use([Navigation]);
@@ -13,7 +14,7 @@ export default function Sharingdeteals() {
   const [sharing, setSharing] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
-  // const [copied, setCopied] = useState(false);
+  const [copied, setCopied] = useState(false);
   // const [contact, setContact] = useState(false);
 
   useEffect(() => {
@@ -58,12 +59,36 @@ export default function Sharingdeteals() {
           <Swiper navigation>
             {sharing.imageUrls.map((imagurl) => (
               <SwiperSlide key={imagurl}>
-                <div className="h-[550px]" style={{background: `linear-gradient(to right, rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0)), url(${imagurl}) center no-repeat`, backgroundSize: "cover",}}>
-                  
+                <div className="h-[550px] relative" style={{background: `linear-gradient(to right, rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0)), url(${imagurl}) center no-repeat`, backgroundSize: "cover",}}>
+                  <div className="text-white text-4xl absolute bottom-10 right-0 left-0 px-5 font-bold">
+                     <h1>{sharing.publicrole}</h1>
+                     <h3 className="text-xs text-gray-500">{sharing.email}</h3>
+                  </div>
                 </div>
               </SwiperSlide>
             ))}
           </Swiper>
+          {/* Liknks sharing */}
+          <div className='fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer'>
+            <FaShare
+              className='text-slate-500'
+              onClick={() => {
+                navigator.clipboard.writeText(window.location.href);
+                setCopied(true);
+                setTimeout(() => {
+                  setCopied(false);
+                }, 2000);
+              }}
+            />
+          </div>
+          {copied && (
+            <p className='fixed top-[23%] right-[5%] z-10 rounded-xl bg-slate-100 p-2'>
+              Success copied!
+            </p>
+          )}
+          <div className="flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4">
+             div.
+          </div>
         </div>
       )}
     </main>
