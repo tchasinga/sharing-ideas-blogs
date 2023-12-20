3146e757503d084a2e88d5626f77b42d417b7102
Tchasinga jacques <tchasingajacques@gmail.com>
2023-12-20 14:36:31 +0200

    new code added

--- frontend/src/Pages/Sharingdeteals.jsx
+++ frontend/src/Pages/Sharingdeteals.jsx
@@ -68,7 +68,7 @@ export default function Sharingdeteals() {
           <Swiper navigation>
             {sharing.imageUrls.map((imagurl) => (
               <SwiperSlide key={imagurl}>
-                <div className="h-[550px] relative" style={{background: `linear-gradient(to right, rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0)), url(${imagurl}) center no-repeat`, backgroundSize: "cover",}}>
+                <div className="h-[550px] relative object-cover" style={{background: `linear-gradient(to right, rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0)), url(${imagurl}) center no-repeat`, backgroundSize: "cover",}}>
                   <div className="text-white text-4xl absolute bottom-10 right-0 left-0 px-5 font-bold">
                      <h1>{sharing.publicrole}</h1>
                      <h3 className="text-xs text-gray-500">{sharing.email}</h3>
