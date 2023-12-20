79fa33b3f82595629fc3f5094ceb0de79975adf0
Tchasinga jacques <tchasingajacques@gmail.com>
2023-12-20 14:28:50 +0200

    new code added

--- frontend/src/Pages/Sharingdeteals.jsx
+++ frontend/src/Pages/Sharingdeteals.jsx
@@ -6,7 +6,8 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 import "swiper/css/bundle";
 import SwiperCore from "swiper";
-import {FaShare,} from 'react-icons/fa';
+import { format } from 'date-fns';
+import {FaShare, FaPhoneAlt , FaRegCalendarAlt} from 'react-icons/fa';
 
 export default function Sharingdeteals() {
   SwiperCore.use([Navigation]);
@@ -41,6 +42,14 @@ export default function Sharingdeteals() {
     fetchSharing();
   }, [params.sharingId]);
 
+  const getMycopid = () => {
+    navigator.clipboard.writeText(window.location.href);
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 2000);
+  }
+
   return (
     <main className="">
       {loading && (
@@ -69,25 +78,42 @@ export default function Sharingdeteals() {
             ))}
           </Swiper>
           {/* Liknks sharing */}
-          <div className='fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer'>
-            <FaShare
-              className='text-slate-500'
-              onClick={() => {
-                navigator.clipboard.writeText(window.location.href);
-                setCopied(true);
-                setTimeout(() => {
-                  setCopied(false);
-                }, 2000);
-              }}
-            />
+        <div className="flex items-center justify-center fixed top-[13%] right-[3%] z-10 tex gap-6">  
+        
+        <div className="flex items-center ">
+              <h1 className="text-white">{sharing.name}</h1>
+           </div>
+
+             <div className="w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer hover:rounded-full">
+             <FaShare className="text-base" onClick={getMycopid}/>
+             </div>
           </div>
           {copied && (
             <p className='fixed top-[23%] right-[5%] z-10 rounded-xl bg-slate-100 p-2'>
               Success copied!
             </p>
           )}
-          <div className="flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4">
-             div.
+          <div className="flex flex-col max-w-6xl mx-auto p-3 my-7 gap-4">
+             <div className="flex items-center gap-5">
+
+             <div className="flex items-center gap-2 text-green-700">
+             <FaPhoneAlt />
+              <span className="text-slate-950 text-sm">{sharing.phonenumber}</span>
+             </div>
+
+          <div className="flex items-center gap-2 text-green-700">
+            <FaRegCalendarAlt />
+            <span className="text-slate-950 text-sm">
+            {format(new Date(sharing.dateinstert), 'dd/MM/yyyy')}
+           </span>
+          </div>
+             </div>
+          <div className="text-slate-700 font-light text-3xl">
+            <h1>{sharing.typeofideas}</h1>
+          </div>
+          <div className="text-slate-950 font-light text-xl">
+            <h1>{sharing.description}</h1>
+            </div>
           </div>
         </div>
       )}
