b74caa9de747a756302f88c42de0a03413ae4898
Tchasinga jacques <tchasingajacques@gmail.com>
2023-12-21 14:28:37 +0200

    Add new code

--- frontend/src/Components/Hearder.jsx
+++ frontend/src/Components/Hearder.jsx
@@ -1,66 +1,33 @@
 import { MdScreenShare } from "react-icons/md";
 import { FaSearch } from "react-icons/fa";
-import { Link, useNavigate, useLocation } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
-import Load from "../Animations/Load";
-import Messagebugs from "../Animations/Messagebugs";
-import SharingCard from "../Pages/SharingCard";
+
  // Assuming NewErrors is the correct component to import
 
 export default function Header() {
   const currentUser = useSelector((state) => state.user && state.user.user.currentUser);
-  const navigate = useNavigate();
-  const location = useLocation();
-
   const [searchTerm, setSearchTerm] = useState('');
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const [sharing, setSharingIdeas] = useState([]);
+  const navigate = useNavigate();
 
-  useEffect(() => {
-    const fetchingListings = async () => {
-      try {
-        setLoading(true);
-        const urlParams = new URLSearchParams(location.search);
-        const searchQuery = urlParams.toString();
-        const res = await fetch(`http://localhost:5000/api/sharing/getallsharingideas?${searchQuery}`);
-        const data = await res.json();
 
-        if (data.success === false) {
-          setError(true);
-          setLoading(false);
-          return;
-        }
-
-        setSharingIdeas(data); // adjust the property name based on your API response
-
-        setLoading(false);
-        setError(false);
-      } catch (error) {
-        setError(true);
-        setLoading(false);
-      }
-    };
-
-    fetchingListings();
-  }, [location.search]);
-
-  const handleSubmit = (e) => {
+   // SUBMITING A FORM TO THE SERVER TO SEACHING DATA AND FILTER THEM
+   const handlerSubmit = (e) => {
     e.preventDefault();
-    const urlParams = new URLSearchParams(location.search);
+    const urlParams = new URLSearchParams(window.location.search);
     urlParams.set('searchTerm', searchTerm);
-    const searchQuery = urlParams.toString();
-    navigate(`/search?${(searchQuery)}`);
-  };
+    const seachQuery = urlParams.toString();
+    navigate(`/search?${seachQuery}`);
+  }
 
-  useEffect(() => {
+  useEffect(() =>{
     const urlParams = new URLSearchParams(location.search);
     const searchTermFromUrl = urlParams.get('searchTerm');
-    if (searchTermFromUrl) {
+    if(searchTermFromUrl){
       setSearchTerm(searchTermFromUrl);
     }
-  }, [location.search]);
+  }, [])
 
   return (
     <div className="px-32 p-2 w-full">
@@ -72,15 +39,19 @@ export default function Header() {
           </div>
         </Link>
 
-        <form onSubmit={handleSubmit} className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form
+          action=""
+          className="bg-slate-100 p-3 rounded-lg flex items-center"
+          onSubmit={handlerSubmit}
+        >
           <input
             type="text"
+            placeholder="Search..."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
-            className="focus:outline-none bg-transparent"
-            placeholder="Search"
           />
-          <button type="submit" className="w-full">
+          <button type="submit" className="text-slate-500">
             <FaSearch />
           </button>
         </form>
@@ -108,20 +79,6 @@ export default function Header() {
         </div>
       </div>
       <div>
-        {loading && <div className='LoadingpageContainer'><Load/></div>}
-        {error && <div className='LoadingpageContainer'><Messagebugs/></div>}
-        <div className="">
-          {!loading && sharing.length === 0 && (
-            <div className="flex flex-col gap-7 LoadingpageContainer">
-              <h1 className="text-3xl font-semibold text-slate-800">No Listing found...</h1>
-            </div>
-          )}
-        </div>
-        <div className="mt-7 justify-center flex gap-5 flex-wrap w-full">
-          {sharing.map((sharingList) => (
-            <SharingCard key={sharingList._id} sharing={sharingList} />
-          ))}
-        </div>
       </div>
     </div>
   );
