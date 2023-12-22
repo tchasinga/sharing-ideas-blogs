07dc9bbd0a7875c0174423e2a64da56fd24139e2
Tchasinga jacques <tchasingajacques@gmail.com>
2023-12-22 16:02:09 +0200

    max width added

--- frontend/src/Pages/Search.jsx
+++ frontend/src/Pages/Search.jsx
@@ -57,7 +57,7 @@ function Search() {
       {sharing.length === 0 && (
         <h1 className="Success">No sharing ideas found</h1>
       )}
-      <div className="gridOneGeneral max-w-7xl mt-7 mx-auto">
+      <div className="flex flex-wrap gap-4 justify-center max-w-7xl mt-7 mx-auto">
         {sharing.map((sharinglist) => (
           <div className="" key={sharinglist._id}>     
           <SharingCard key={sharinglist._id} sharinglist={sharinglist} />
--- frontend/src/Pages/SharingCard.jsx
+++ frontend/src/Pages/SharingCard.jsx
@@ -1,23 +1,28 @@
 import PropTypes from "prop-types";
-import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function SharingCard({ sharinglist }) {
-  const [color, setColor] = useState('black');
 
-  const toggleColor = () => {
-    setColor(prevColor => prevColor === 'black' ? 'white' : 'black');
-  };
+ 
 
   return (
-    <div className="flex flex-col justify-center items-center bg-gray-100 rounded-lg p-3" onClick={toggleColor}>
+    <div className="flex flex-col justify-center items-center maxWidth bg-gray-100 rounded-lg p-3">
       <div className="flex flex-col justify-between relative items-center">
         <img src={sharinglist.imageUrls[0]} className="h-[250px] w-full relative object-cover" alt="" />
-        <div className="absolute text-white font-semibold bottom-10 right-0 left-0 px-5 text-sm bg-slate-300 p-2">
+        <div className="absolute text-white font-semibold bottom-0 right-0 left-0 px-5 text-sm bg-slate-300 p-2">
           <h1>{sharinglist.name}</h1>
-          <h3 className={`text-xs changin text-${color}`}>{sharinglist.email}</h3>
+          <h3 className={`text-xs font-normal text-gray-300`}>{sharinglist.email}</h3>
         </div>
       </div>
+      <div className="flex flex-col w-full">
+        <div className="flex flex-col justify-between w-full mb-3 px-3">
+          <h1 className="text-xl font-semibold text-slate-950">{sharinglist.publicrole}</h1>
+          <h3 className="text-xs font-normal text-gray-500">{sharinglist.typeofideas}</h3>
+        </div>
+        <div className="flex flex-col justify-center  px-3">
+          <p className="text-slate-950 text-sm font-normal line-clamp-3">{sharinglist.description}</p>
+        </div>
+    </div>
     </div>
   );
 }
