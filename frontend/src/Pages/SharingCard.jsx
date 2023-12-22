a987be5603b17a0038bd0cebbe2bac888743e9f4
Tchasinga jacques <tchasingajacques@gmail.com>
2023-12-22 15:22:20 +0200

    code reiview

--- frontend/src/Pages/SharingCard.jsx
+++ frontend/src/Pages/SharingCard.jsx
@@ -1,13 +1,22 @@
-
 import PropTypes from "prop-types";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function SharingCard({ sharinglist }) {
+  const [color, setColor] = useState('black');
+
+  const toggleColor = () => {
+    setColor(prevColor => prevColor === 'black' ? 'white' : 'black');
+  };
+
   return (
-    <div className="flex flex-col justify-center  items-center  bg-gray-100 rounded-lg p-3">
+    <div className="flex flex-col justify-center items-center bg-gray-100 rounded-lg p-3" onClick={toggleColor}>
       <div className="flex flex-col justify-between relative items-center">
         <img src={sharinglist.imageUrls[0]} className="h-[250px] w-full relative object-cover" alt="" />
-        <h1 className="absolute text-white font-semibold bottom-10 right-0 left-0 px-5 text-sm">{sharinglist.name}</h1>
+        <div className="absolute text-white font-semibold bottom-10 right-0 left-0 px-5 text-sm bg-slate-300 p-2">
+          <h1>{sharinglist.name}</h1>
+          <h3 className={`text-xs changin text-${color}`}>{sharinglist.email}</h3>
+        </div>
       </div>
     </div>
   );
