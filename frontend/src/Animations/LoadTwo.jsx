38d478f2b7eed4e33c1836fa2ef7f60d5ed46e01
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-14 11:46:20 +0200

    Load

---
+++ frontend/src/Animations/LoadTwo.jsx
@@ -0,0 +1,13 @@
+import { tailChase } from "ldrs";
+
+tailChase.register();
+
+export default function LoadTwo() {
+  return (
+    <div>
+      <l-tail-chase size="40" speed="1.75" color="black"></l-tail-chase>
+    </div>
+  );
+}
+
+// Default values shown
