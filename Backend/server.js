00d6e3e1b59c4f43431371742803bcf94b9e10f9
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-22 19:41:04 +0200

    new code is added

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 // Middlewares to parse JSON
 app.use(express.json());
 
-// Error handling middleware
+// Error handling middleware...
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error!";
