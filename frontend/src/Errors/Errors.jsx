c449b05173fc91bacbe4f381cb2e47927adb3658
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-27 12:07:11 +0200

    new code

--- frontend/src/Errors/Errors.jsx
+++ frontend/src/Errors/Errors.jsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
 import Looser from "../Tasks/Looser";
+import { MdCancel } from "react-icons/md";
 
 export default function Errors() {
+
+  const [cancelErrorsWindows,  setCancelErrorsWindows] = useState(false)
+
+  const handlerCancleScreeen = () => {
+    setCancelErrorsWindows(false)
+  }
+
   return (
-    <div className="flex items-center justify-center flex-col gap-2 Successigner bg-red-100">
-      <div className="">
-          <Looser/>
-      </div>
-     <div>
-     <p className="text-slate-950 text-xs">Please check your Password or email</p>
-     </div>
-  </div>
+    <div>
+      {cancelErrorsWindows && (
+        <div className="flex max-w-xs mx-auto items-center justify-center flex-col gap-2 Successigner bg-red-100">
+          <MdCancel  className="text-black text-xl" onClick={handlerCancleScreeen}/>
+          <div className="">
+            <Looser/>
+          </div>
+          <div>
+            <p className="text-slate-950 text-xs">Please check your Password or email</p>
+          </div>
+        </div>
+      )}
+    </div>
   )
 }
