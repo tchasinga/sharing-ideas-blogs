f53f96a5b76c33045a7e13bdb75f53899ecee38c
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-27 11:49:13 +0200

    new

--- frontend/src/Tasks/Looser.jsx
+++ frontend/src/Tasks/Looser.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import Lottie from "lottie-react";
+import successFaild from "../Lotties/Animation - 1706348696617Looser.json";
+
+
 
 export default function Looser() {
   return (
     <div>
-      
+      <Lottie className="lottie-container" animationData={successFaild} />
     </div>
   )
 }
