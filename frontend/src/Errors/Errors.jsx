13a6128a057bfb9c5b7f2e6a907c8958e004b53b
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-27 11:51:05 +0200

    New code is added

--- frontend/src/Errors/Errors.jsx
+++ frontend/src/Errors/Errors.jsx
@@ -1,8 +1,14 @@
+import Looser from "../Tasks/Looser";
 
 export default function Errors() {
   return (
-    <div className="flex items-center flex-col gap-2 Successigner bg-red-100">
-    <p className="text-slate-950 text-xs">Please check your Password or email</p>
+    <div className="flex items-center justify-center flex-col gap-2 Successigner bg-red-100">
+      <div className="">
+          <Looser/>
+      </div>
+     <div>
+     <p className="text-slate-950 text-xs">Please check your Password or email</p>
+     </div>
   </div>
   )
 }
