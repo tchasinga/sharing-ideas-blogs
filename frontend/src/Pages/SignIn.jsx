eb9498cdc78b0d0c5d6f0a76e17b3d9f9dd8760c
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-01 12:33:11 +0200

    new new updating

--- frontend/src/Pages/SignIn.jsx
+++ frontend/src/Pages/SignIn.jsx
@@ -1,48 +1,49 @@
 import { TbWorldShare } from "react-icons/tb";
 import pageImg from "../Img/cheerful.png";
-import { TextField ,Button , Tooltip} from '@mui/material';
-import {Link, useNavigate} from 'react-router-dom'
-import {useDispatch, useSelector} from 'react-redux'
-import {signInStart,singInSuccess,signInFailure} from '../redux/user/userSlice.js'
+import { TextField, Button, Tooltip } from '@mui/material';
+import { Link, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { signInStart, singInSuccess, signInFailure } from '../redux/user/userSlice.js'
 import { useState, } from 'react';
 import Success from '../Tasks/Success.jsx'
 import Errors from "../Errors/Errors.jsx";
+import Load from "../Animations/Load.jsx";
 
 
 
 
 export default function SignIn() {
-   
- const [formData, setFormData] = useState({})
- const {loading } = useSelector(state =>  state.user && state.user.user)
- const [showError, setShowError] = useState(false); // New state for error popup
- const [showSuccess, setShowSuccess] = useState(false); // New state for success popup
- const dispatch = useDispatch()
- const navigate = useNavigate()
+
+  const [formData, setFormData] = useState({})
+  const { loading } = useSelector(state => state.user && state.user.user)
+  const [showError, setShowError] = useState(false); // New state for error popup
+  const [showSuccess, setShowSuccess] = useState(false); // New state for success popup
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
   const handleChange = (e) => {
-    setFormData({...formData , [e.target.id] : e.target.value})
+    setFormData({ ...formData, [e.target.id]: e.target.value })
   }
 
   const handlerSubmit = async (e) => {
     e.preventDefault()
     try {
-       dispatch(signInStart())
+      dispatch(signInStart())
 
-       const res = await fetch('https://blogs-sharing-ideas-api.onrender.com/api/auth/signin', {
+      const res = await fetch('https://blogs-sharing-ideas-api.onrender.com/api/auth/signin', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify(formData)
-       })
-       const data = await res.json()
-       if(data.success === false){
+      })
+      const data = await res.json()
+      if (data.success === false) {
         dispatch(signInFailure(setShowError(true)))
         return
-       }
-        dispatch(singInSuccess(data))
-         // Show the Success component
+      }
+      dispatch(singInSuccess(data))
+      // Show the Success component
       setShowSuccess(true);
       setShowError(false); // Hide the Error component
 
@@ -61,11 +62,11 @@ export default function SignIn() {
 
   return (
     <div className="bg-blue-100  mainBody">
-       <div className="newproductgrid max-w-7xl mx-auto rounded-3xl">
+      <div className="newproductgrid max-w-7xl mx-auto rounded-3xl">
         <div className="firstDiv p-3 noneget">
           <div className="flex items-center gap-1 pl-3">
-          <TbWorldShare className="text-2xl text-white"/>
-          <h2 className="text-sm text-indigo-50 font-medium">Welcom in our blogs sharing</h2>
+            <TbWorldShare className="text-2xl text-white" />
+            <h2 className="text-sm text-indigo-50 font-medium">Welcom in our blogs sharing</h2>
           </div>
           <div className="flex justify-center getBorder items-center">
             <img className="pl-20 pt-14 w-full" src={pageImg} alt="" />
@@ -76,29 +77,36 @@ export default function SignIn() {
         </div>
 
         {/* Second side in downloading now  */}
-         
+
         <div className="secondDiv w-full">
-          
+
           <Link to='/signup'>
-          <div className="mt-5 flex justify-end pr-5">
-          <Tooltip title="Click here to create an account" arrow placement='left'>
-            <Button variant="contained">Sing up</Button>
-            </Tooltip>
-          </div>
+            <div className="mt-5 flex justify-end pr-5">
+              <Tooltip title="Click here to create an account" arrow placement='left'>
+                <Button variant="contained">Sing up</Button>
+              </Tooltip>
+            </div>
           </Link>
 
           <form onSubmit={handlerSubmit} className="mt-44 w-full flex flex-col  items-center gap-3">
-           <div className="flex ml-20 uppercase text-xl font-bold w-full">
-           <h1 className="noneget">Sing in here</h1>
-           </div>
-          <TextField  label="set your email" required className="w-2/3" id="email" onChange={handleChange} variant='outlined' type='email'/>
-          <TextField  label="set your password" id="password" onChange={handleChange} helperText="don't share your password" className="w-2/3" variant='outlined' type='password'/>
-          <Button type="submit" variant="contained" disabled={loading}>{loading ? 'loading...' : 'Sing-in'}</Button>
+            <div className="flex ml-20 uppercase text-xl font-bold w-full">
+              <h1 className="noneget">Sing in here</h1>
+            </div>
+            <TextField label="set your email" required className="w-2/3" id="email" onChange={handleChange} variant='outlined' type='email' />
+            <TextField label="set your password" id="password" onChange={handleChange} helperText="don't share your password" className="w-2/3" variant='outlined' type='password' />
+            <Button type="submit" variant="contained" disabled={loading}>{
+              loading ?
+                <div className="flex items-center justify-center gap-2">
+                  <span className="text-xs"><Load/></span>
+                  <p>Loading...</p>
+                </div> : "Sing-In now"
+            }
+            </Button>
           </form>
           {showSuccess && <Success />} {/* Show success popup */}
         </div>
-        {showError && <Errors/>}
-        </div>
+        {showError && <Errors />}
+      </div>
     </div>
   )
 }
--- frontend/src/Pages/SignUp.jsx
+++ frontend/src/Pages/SignUp.jsx
@@ -1,9 +1,10 @@
 import { TbWorldShare } from "react-icons/tb";
 import pageImg from "../Img/cheerful.png";
-import { TextField ,Button , Tooltip} from '@mui/material';
-import {Link, useNavigate} from 'react-router-dom'
+import { TextField, Button, Tooltip } from '@mui/material';
+import { Link, useNavigate } from 'react-router-dom'
 import { useState } from "react";
 import Success from "../Tasks/Success";
+import Load from "../Animations/Load";
 
 
 export default function SignUp() {
@@ -53,48 +54,55 @@ export default function SignUp() {
     }
   }
 
-    return (
-      <div className="bg-blue-100  mainBody">
-         <div className="newproductgrid max-w-7xl mx-auto rounded-3xl">
-          <div className="firstDiv p-3 noneget">
-            <div className="flex items-center gap-1 pl-3">
-            <TbWorldShare className="text-2xl text-white"/>
+  return (
+    <div className="bg-blue-100  mainBody">
+      <div className="newproductgrid max-w-7xl mx-auto rounded-3xl">
+        <div className="firstDiv p-3 noneget">
+          <div className="flex items-center gap-1 pl-3">
+            <TbWorldShare className="text-2xl text-white" />
             <h2 className="text-sm text-indigo-50 font-medium">Welcom in our blogs sharing</h2>
-            </div>
-            <div className="flex justify-center getBorder items-center">
-              <img className="pl-20 pt-14 w-full" src={pageImg} alt="" />
-            </div>
-            <div className="flex justify-center items-center mt-32">
-              <p className="text-xs text-white">Copyright @2023 My creation All good deserved</p>
-            </div>
           </div>
-  
-          {/* Second side in downloading now  */}
-           
-          <div className="secondDiv w-full">
-            <Link to='/signin'>
+          <div className="flex justify-center getBorder items-center">
+            <img className="pl-20 pt-14 w-full" src={pageImg} alt="" />
+          </div>
+          <div className="flex justify-center items-center mt-32">
+            <p className="text-xs text-white">Copyright @2023 My creation All good deserved</p>
+          </div>
+        </div>
+
+        {/* Second side in downloading now  */}
+
+        <div className="secondDiv w-full">
+          <Link to='/signin'>
             <div className="mt-5 flex justify-end pr-5">
-            <Tooltip title="Click here to login now" arrow placement='left'>
-              <Button variant="contained">Sing in</Button>
+              <Tooltip title="Click here to login now" arrow placement='left'>
+                <Button variant="contained">Sing in</Button>
               </Tooltip>
             </div>
-            </Link>
-  
-            <form onSubmit={handlerSubmit} className="mt-40 w-full flex flex-col  items-center gap-3">
-             <div className="flex pr-24 uppercase text-xl font-bold">
-             <h1 className="">create your account here</h1>
-             </div>
-            <TextField  label="set your username" required className="w-2/3" id='username' onChange={handleChange} variant='outlined' type='text'/>
-            <TextField  label="set your email" required className="w-2/3" id='email' onChange={handleChange} variant='outlined' type='text'/>
-            <TextField  label="set your password" required helperText="don't share your password" id='password' onChange={handleChange} className="w-2/3" variant='outlined' type='password'/>
-            <Button type="submit" disabled={loading} variant="contained">{loading ? 'loading...' : 'Sign up'}</Button>
-            </form>
-            {showSuccess && <Success />} {/* Show success popup */}
-             <div className="flex gap-2 mt-3 text-xs">
-              {error && <p className="text-red-500">{error}</p>}
-             </div>
+          </Link>
+
+          <form onSubmit={handlerSubmit} className="mt-40 w-full flex flex-col  items-center gap-3">
+            <div className="flex pr-24 uppercase text-xl font-bold">
+              <h1 className="">create your account here</h1>
+            </div>
+            <TextField label="set your username" required className="w-2/3" id='username' onChange={handleChange} variant='outlined' type='text' />
+            <TextField label="set your email" required className="w-2/3" id='email' onChange={handleChange} variant='outlined' type='text' />
+            <TextField label="set your password" required helperText="don't share your password" id='password' onChange={handleChange} className="w-2/3" variant='outlined' type='password' />
+            <Button type="submit" disabled={loading} variant="contained">{
+              loading ?
+                <div className="flex items-center justify-center gap-2">
+                   <span className="text-xs"><Load/></span>
+                  <p>Loading...</p>
+                </div>
+                :
+                'Sign up'}</Button>
+          </form>
+          {showSuccess && <Success />} {/* Show success popup */}
+          <div className="flex gap-2 mt-3 text-xs">
+            {error && <p className="text-red-500">{error}</p>}
           </div>
-          </div>
+        </div>
       </div>
-    )
-  }
+    </div>
+  )
+}
