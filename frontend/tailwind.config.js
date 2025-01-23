32e475a3a226e84da5d841421b3482a0908714e1
Tchasinga jacques <tchasingajacques@gmail.com>
2025-01-23 10:15:29 +0300

    fix: Update Tailwind CSS imports and configuration for improved structure and compatibility

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,13 @@
+/* global require */
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,html}',
+  ],
   theme: {
     extend: {},
   },
   plugins: [
     require('@tailwindcss/line-clamp'),
   ],
-};
+}
